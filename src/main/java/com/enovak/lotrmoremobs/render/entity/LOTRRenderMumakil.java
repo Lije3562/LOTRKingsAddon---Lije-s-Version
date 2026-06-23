2cfdae04807e3917f065ced6d6fa905a1b5f1d4e
Lije3562 <elijah.r.novak@gmail.com>
2026-06-22 20:26:35 -0500

    Use standard renderer for Mumakil

--- src/main/java/com/enovak/lotrmoremobs/render/entity/LOTRRenderMumakil.java
+++ src/main/java/com/enovak/lotrmoremobs/render/entity/LOTRRenderMumakil.java
@@ -1,27 +1,20 @@
 package com.enovak.lotrmoremobs.render.entity;
 
-import com.enovak.lotrmoremobs.entity.animal.LOTREntityMumakil;
-import com.enovak.lotrmoremobs.model.mumakil.LOTRGeoModelMumakil;
+import com.enovak.lotrmoremobs.model.LOTRModelMumakil;
+import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
-import software.bernie.geckolib3.core.util.Color;
-import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
-public class LOTRRenderMumakil extends GeoEntityRenderer<LOTREntityMumakil> {
-    private static final ResourceLocation MUMAKIL_WAR_TEXTURE =
+public class LOTRRenderMumakil extends RenderLiving {
+    private static final ResourceLocation MUMAKIL_TEXTURE =
             new ResourceLocation("lotrmoremobs", "textures/mob/mumakil/mumakil_war.png");
 
     public LOTRRenderMumakil() {
-        super(new LOTRGeoModelMumakil());
-        this.shadowSize = 0.5F;
+        super(new LOTRModelMumakil(), 1.5F);
     }
 
     @Override
     protected ResourceLocation getEntityTexture(Entity entity) {
-        return MUMAKIL_WAR_TEXTURE;
-    }
-
-    public Color getRenderColor(LOTREntityMumakil animatable, float partialTicks) {
-        return Color.ofRGBA(255, 255, 255, 255);
+        return MUMAKIL_TEXTURE;
     }
 }
