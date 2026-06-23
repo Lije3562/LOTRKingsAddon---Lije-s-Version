2707b0cd6d77d96ba08b9e4a29b9b49862e8127c
Lije3562 <elijah.r.novak@gmail.com>
2026-06-23 18:52:38 -0500

    Scale Mumakil RenderLiving renderer

--- src/main/java/com/enovak/lotrmoremobs/render/entity/LOTRRenderMumakil.java
+++ src/main/java/com/enovak/lotrmoremobs/render/entity/LOTRRenderMumakil.java
@@ -3,14 +3,22 @@ package com.enovak.lotrmoremobs.render.entity;
 import com.enovak.lotrmoremobs.model.LOTRModelMumakil;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 
 public class LOTRRenderMumakil extends RenderLiving {
     private static final ResourceLocation MUMAKIL_TEXTURE =
             new ResourceLocation("lotrmoremobs", "textures/mob/mumakil/mumakil_war.png");
+    private static final float MUMAKIL_RENDER_SCALE = 1.35F;
 
     public LOTRRenderMumakil() {
-        super(new LOTRModelMumakil(), 1.5F);
+        super(new LOTRModelMumakil(), 2.0F);
+    }
+
+    @Override
+    protected void preRenderCallback(EntityLivingBase entity, float partialTickTime) {
+        GL11.glScalef(MUMAKIL_RENDER_SCALE, MUMAKIL_RENDER_SCALE, MUMAKIL_RENDER_SCALE);
     }
 
     @Override
