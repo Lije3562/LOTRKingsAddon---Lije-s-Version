b93b45e806a14032da6b14680d7607a12343311c
Lije3562 <elijah.r.novak@gmail.com>
2026-06-22 19:06:51 -0500

    Type Mumakil GeckoLib renderer

--- src/main/java/com/enovak/lotrmoremobs/render/entity/LOTRRenderMumakil.java
+++ src/main/java/com/enovak/lotrmoremobs/render/entity/LOTRRenderMumakil.java
@@ -1,17 +1,11 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package com.enovak.lotrmoremobs.render.entity;
 
 import com.enovak.lotrmoremobs.entity.animal.LOTREntityMumakil;
 import com.enovak.lotrmoremobs.model.mumakil.LOTRGeoModelMumakil;
-
 import software.bernie.geckolib3.core.util.Color;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
-public class LOTRRenderMumakil extends GeoEntityRenderer {
+public class LOTRRenderMumakil extends GeoEntityRenderer<LOTREntityMumakil> {
     public LOTRRenderMumakil() {
         super(new LOTRGeoModelMumakil());
         this.shadowSize = 0.5F;
