ef691e4988e3bf1ae713e0590937bd50221aa0fd
Lije3562 <elijah.r.novak@gmail.com>
2026-06-22 20:26:45 -0500

    Remove GeckoLib init from Mumakil client proxy

--- src/main/java/com/enovak/lotrmoremobs/proxy/ClientProxy.java
+++ src/main/java/com/enovak/lotrmoremobs/proxy/ClientProxy.java
@@ -3,12 +3,10 @@ package com.enovak.lotrmoremobs.proxy;
 import com.enovak.lotrmoremobs.entity.animal.LOTREntityMumakil;
 import com.enovak.lotrmoremobs.render.entity.LOTRRenderMumakil;
 import cpw.mods.fml.client.registry.RenderingRegistry;
-import software.bernie.geckolib3.GeckoLib;
 
 public class ClientProxy extends CommonProxy {
     @Override
     public void registerRenderers() {
-        GeckoLib.initialize();
         RenderingRegistry.registerEntityRenderingHandler(LOTREntityMumakil.class, new LOTRRenderMumakil());
     }
 }
