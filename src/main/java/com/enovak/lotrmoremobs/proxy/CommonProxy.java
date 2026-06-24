3d6ed7bba87e982bde7ca24bb668e50841bad915
Lije3562 <elijah.r.novak@gmail.com>
2026-06-24 11:48:07 -0500

    Register Mumakil fear event handler

--- src/main/java/com/enovak/lotrmoremobs/proxy/CommonProxy.java
+++ src/main/java/com/enovak/lotrmoremobs/proxy/CommonProxy.java
@@ -1,6 +1,13 @@
 package com.enovak.lotrmoremobs.proxy;
 
+import com.enovak.lotrmoremobs.handler.MumakilFearEventHandler;
+import net.minecraftforge.common.MinecraftForge;
+
 public class CommonProxy {
     public void registerRenderers() {
+    }
+
+    public void registerEventHandlers() {
+        MinecraftForge.EVENT_BUS.register(new MumakilFearEventHandler());
     }
 }
