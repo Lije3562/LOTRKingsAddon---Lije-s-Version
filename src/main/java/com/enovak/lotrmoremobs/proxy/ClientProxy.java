8e6f70fc2dfa1a791a37fb76bc4decf178faa848
Lije3562 <elijah.r.novak@gmail.com>
2026-06-24 13:11:09 -0500

    Use Mumakil Geo renderer on experiment branch

--- src/main/java/com/enovak/lotrmoremobs/proxy/ClientProxy.java
+++ src/main/java/com/enovak/lotrmoremobs/proxy/ClientProxy.java
@@ -1,12 +1,15 @@
 package com.enovak.lotrmoremobs.proxy;
 
 import com.enovak.lotrmoremobs.entity.animal.LOTREntityMumakil;
-import com.enovak.lotrmoremobs.render.entity.LOTRRenderMumakil;
+import com.enovak.lotrmoremobs.render.entity.LOTRRenderMumakilGeo;
 import cpw.mods.fml.client.registry.RenderingRegistry;
+import software.bernie.geckolib3.GeckoLib;
 
 public class ClientProxy extends CommonProxy {
     @Override
     public void registerRenderers() {
-        RenderingRegistry.registerEntityRenderingHandler(LOTREntityMumakil.class, new LOTRRenderMumakil());
+        GeckoLib.initialize();
+        // Experiment branch only: swap this back to LOTRRenderMumakil to return to the stable ModelBase renderer.
+        RenderingRegistry.registerEntityRenderingHandler(LOTREntityMumakil.class, new LOTRRenderMumakilGeo());
     }
 }
