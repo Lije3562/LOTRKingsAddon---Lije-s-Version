4c13be768ba7ddd21c729a257862029b0575df37
Lije3562 <elijah.r.novak@gmail.com>
2026-06-29 17:30:06 -0500

    Use scaled Mumakil inventory preview renderer

--- src/main/java/com/enovak/lotrmoremobs/proxy/ClientProxy.java
+++ src/main/java/com/enovak/lotrmoremobs/proxy/ClientProxy.java
@@ -1,7 +1,7 @@
 package com.enovak.lotrmoremobs.proxy;
 
 import com.enovak.lotrmoremobs.entity.animal.LOTREntityMumakil;
-import com.enovak.lotrmoremobs.render.entity.LOTRRenderMumakilGeo;
+import com.enovak.lotrmoremobs.render.entity.LOTRRenderMumakilGeoInventoryScaled;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import software.bernie.geckolib3.GeckoLib;
 
@@ -9,7 +9,7 @@ public class ClientProxy extends CommonProxy {
     @Override
     public void registerRenderers() {
         GeckoLib.initialize();
-        // Experiment branch only: swap this back to LOTRRenderMumakil to return to the stable ModelBase renderer.
-        RenderingRegistry.registerEntityRenderingHandler(LOTREntityMumakil.class, new LOTRRenderMumakilGeo());
+        // Uses the stable Geo renderer path, with a wrapper that only shrinks the inventory preview.
+        RenderingRegistry.registerEntityRenderingHandler(LOTREntityMumakil.class, new LOTRRenderMumakilGeoInventoryScaled());
     }
 }
