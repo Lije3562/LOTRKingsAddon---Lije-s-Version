bfc339e7a8687d1afdf065dfe48cc4d30d3dd1bb
Lije3562 <elijah.r.novak@gmail.com>
2026-07-01 14:45:21 -0500

    Explicitly register Mumakil tusk item icon

--- src/main/java/com/enovak/lotrmoremobs/item/LOTRItemMumakilTusk.java
+++ src/main/java/com/enovak/lotrmoremobs/item/LOTRItemMumakilTusk.java
@@ -1,14 +1,26 @@
 package com.enovak.lotrmoremobs.item;
 
+import com.enovak.lotrmoremobs.Main;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class LOTRItemMumakilTusk extends Item {
+    private static final String TUSK_ICON_NAME = Main.MODID + ":mumakil_tusk";
 
     public LOTRItemMumakilTusk() {
         this.setMaxStackSize(64);
         this.setCreativeTab(CreativeTabs.tabMaterials);
         this.setUnlocalizedName("mumakil_tusk");
-        this.setTextureName("lotrmoremobs:mumakil_tusk");
+        this.setTextureName(TUSK_ICON_NAME);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister iconRegister) {
+        System.out.println("[LOTRMoreMobs] Registering Mumakil tusk item icon: " + TUSK_ICON_NAME);
+        this.itemIcon = iconRegister.registerIcon(TUSK_ICON_NAME);
     }
 }
