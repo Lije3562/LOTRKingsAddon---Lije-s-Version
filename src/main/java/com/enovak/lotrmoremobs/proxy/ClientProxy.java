9ba2132111b55416a5afe9ec43be782c7082a46c
Lije3562 <elijah.r.novak@gmail.com>
2026-07-03 21:58:53 -0500

    Render custom Mumakil howdah archers as Near Haradrim archers

--- src/main/java/com/enovak/lotrmoremobs/proxy/ClientProxy.java
+++ src/main/java/com/enovak/lotrmoremobs/proxy/ClientProxy.java
@@ -2,9 +2,13 @@ package com.enovak.lotrmoremobs.proxy;
 
 import com.enovak.lotrmoremobs.client.MumakilInventoryKeyHandler;
 import com.enovak.lotrmoremobs.entity.animal.LOTREntityMumakil;
+import com.enovak.lotrmoremobs.entity.npc.LOTREntityMumakilHowdahArcher;
 import com.enovak.lotrmoremobs.render.entity.LOTRRenderMumakilGeoInventoryScaled;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.FMLCommonHandler;
+import lotr.common.entity.npc.LOTREntityNearHaradrimArcher;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraftforge.common.MinecraftForge;
 import software.bernie.geckolib3.GeckoLib;
 
@@ -19,10 +23,25 @@ public class ClientProxy extends CommonProxy {
                 new LOTRRenderMumakilGeoInventoryScaled()
         );
 
+        this.registerHowdahArcherRenderer();
 
         MumakilInventoryKeyHandler mumakilInventoryKeyHandler = new MumakilInventoryKeyHandler();
         FMLCommonHandler.instance().bus().register(mumakilInventoryKeyHandler);
         MinecraftForge.EVENT_BUS.register(mumakilInventoryKeyHandler);
 
+    }
+
+    private void registerHowdahArcherRenderer() {
+        Render render = (Render)RenderManager.instance.entityRenderMap.get(LOTREntityNearHaradrimArcher.class);
+
+        if (render != null) {
+            RenderingRegistry.registerEntityRenderingHandler(
+                    LOTREntityMumakilHowdahArcher.class,
+                    render
+            );
+            System.out.println("[LOTRMoreMobs] Registered Mumakil howdah archer renderer from Near Haradrim archer renderer.");
+        } else {
+            System.out.println("[LOTRMoreMobs] Could not find Near Haradrim archer renderer for Mumakil howdah archers.");
+        }
     }
 }
