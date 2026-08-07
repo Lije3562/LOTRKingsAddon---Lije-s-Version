fa2a848ec68e567d9808a2c3c050f8cf2d852685
Elijah Novak <elijah.r.novak@gmail.com>
2026-08-07 18:06:01 -0500

    Fix Mumakil GUI packet server-thread handoff

--- src/main/java/com/enovak/lotrmoremobs/network/MumakilOpenGuiPacket.java
+++ src/main/java/com/enovak/lotrmoremobs/network/MumakilOpenGuiPacket.java
@@ -1,11 +1,17 @@
 package com.enovak.lotrmoremobs.network;
 
 import com.enovak.lotrmoremobs.entity.animal.LOTREntityMumakil;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayerMP;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class MumakilOpenGuiPacket implements IMessage {
 
@@ -18,19 +24,53 @@ public class MumakilOpenGuiPacket implements IMessage {
     }
 
     public static class Handler implements IMessageHandler<MumakilOpenGuiPacket, IMessage> {
+        private final Queue<EntityPlayerMP> pendingPlayers = new ConcurrentLinkedQueue<EntityPlayerMP>();
+
+        public Handler() {
+            FMLCommonHandler.instance().bus().register(this);
+        }
+
         @Override
         public IMessage onMessage(MumakilOpenGuiPacket message, MessageContext ctx) {
             EntityPlayerMP player = ctx.getServerHandler().playerEntity;
 
-            if (player != null && player.ridingEntity instanceof LOTREntityMumakil) {
+            if (player != null) {
+                this.pendingPlayers.offer(player);
+            }
+
+            return null;
+        }
+
+        @SubscribeEvent
+        public void onServerTick(TickEvent.ServerTickEvent event) {
+            if (event.phase != TickEvent.Phase.START) {
+                return;
+            }
+
+            EntityPlayerMP player;
+            while ((player = this.pendingPlayers.poll()) != null) {
+                if (player.isDead
+                        || !player.isEntityAlive()
+                        || player.worldObj == null
+                        || player.worldObj.isRemote
+                        || player.worldObj.getEntityByID(player.getEntityId()) != player
+                        || !(player.ridingEntity instanceof LOTREntityMumakil)) {
+                    continue;
+                }
+
                 LOTREntityMumakil mumakil = (LOTREntityMumakil) player.ridingEntity;
+                if (mumakil.isDead
+                        || !mumakil.isEntityAlive()
+                        || mumakil.worldObj != player.worldObj
+                        || mumakil.worldObj.getEntityByID(mumakil.getEntityId()) != mumakil
+                        || !mumakil.canPlayerUseMumakilInventory(player)) {
+                    continue;
+                }
 
                 System.out.println("[LOTRMoreMobs] Server received Mumakil open GUI packet. Opening GUI.");
 
                 mumakil.openGUI(player);
             }
-
-            return null;
         }
     }
 }
